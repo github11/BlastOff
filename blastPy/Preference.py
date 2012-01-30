ce45a36c3f491ceff0c742f8780bdd366b6911fa
arh <anthrond@gmail.com>
2012-01-30 14:56:42 -0700

    Adding python back-end classes

---
+++ blastPy/Preference.py
@@ -0,0 +1,47 @@
+class Preference:
+
+    def __init__(self):
+        self._name = "Default Preference Name"
+        self._command_text = "default-command-text"
+        self._value = "default-value"
+
+    @classmethod
+    def from_args(self, friendly_name, command_text, value):
+        new_pref = Preference()
+        new_pref._name = friendly_name
+        new_pref._command_text = command_text
+        new_pref._value = value
+        return new_pref
+
+    @property
+    def name(self):
+        """The user-friendly name for a preference."""
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value
+    @name.deleter
+    def name(self):
+        del self._name
+
+    @property
+    def command_text(self):
+        """The actuall parameter text that will be emmited with the"""
+        """command"""
+        return self._command_text
+    @command_text.setter
+    def command_text(self, value):
+        self._command_text = value
+    @command_text.deleter
+    def command_text(self):
+        del self._command_text
+
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+    @value.deleter
+    def value(self):
+        del self._value
---
+++ blastPy/__init__.py
@@ -0,0 +1,4 @@
+from Preference import Preference
+from Shot import Shot
+from Production import Production
+from CommandLinePrinter import CommandLinePrinter
